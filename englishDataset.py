b0c0ace262354d96b2a428e2feb1068de85ec9c1
MaiAbboud <ma1991abb@gmail.com>
2022-03-01 14:16:23 +0200

    add train option to load train/test data seperatly compare time with/out dataloader class

--- englishDataset.py
+++ englishDataset.py
@@ -1,4 +1,5 @@
 import os
+from time import time
 # from pyrsistent import optional
 from torchvision.io import read_image
 from torchvision.datasets import VisionDataset
@@ -8,17 +9,20 @@ from torchvision.transforms import ToTensor,Grayscale
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import time
+from torch.utils.data import dataloader
 
 class englishDataset(VisionDataset):
     def __init__(
         self, 
-        root: str, 
+        root: str,
+        train: bool = True, 
         transforms: Optional[Callable] = None, 
         transform: Optional[Callable] = None, 
         target_transform: Optional[Callable] = None
         ) -> None:
         super().__init__(root, transforms, transform, target_transform)
-
+        self.train = train
         #read .csv data file 
         data_csv_file = pd.read_csv(root+'/english.csv')
         data_size = data_csv_file.shape[0]
@@ -34,21 +38,42 @@ class englishDataset(VisionDataset):
     
     def __getitem__(self, index: int) -> Any:
         # read image for specific data
-        img_pth = os.path.join(self.root , self.dataset_train['image'][index]) 
-        img , label = read_image(img_pth) , self.dataset_train['label'][index]
+        if self.train:
+            img_pth = os.path.join(self.root , self.dataset_train['image'][index]) 
+            img , label = read_image(img_pth) , self.dataset_train['label'][index]
+        else:
+            img_pth = os.path.join(self.root , self.dataset_test['image'][index]) 
+            img , label = read_image(img_pth) , self.dataset_test['label'][index]
         return img,label
 
     def __len__(self) -> int:
-        train_size = self.dataset_train.shape[0]
-        test_size = self.dataset_test.shape[0]
-        return train_size,test_size
+        if self.train:
+            train_size = self.dataset_train.shape[0]
+            return train_size
+        else :
+            test_size = self.dataset_test.shape[0]
+            return test_size
 
 
 dataset = englishDataset(
     root = 'data/english_dataset',
+    train=True
 )
-train_size = dataset.dataset_train.shape[0]
-test_size = dataset.dataset_test.shape[0]
+train_size = len(dataset)
+
+#load all dataset time without dataloader
+start_time= time.time()
+for sample in dataset.dataset_train:
+    print(sample)
+end_time = time.time()
+print(f"time without dataloader class is : {end_time - start_time}")
+
+# with dataloader 
+batch_size = 1
+start_time= time.time()
+train_dataloader = dataloader.DataLoader(dataset=dataset,batch_size=batch_size)
+end_time = time.time()
+print(f"time with dataloader class is : {end_time - start_time}")
 
 
 # show training data
@@ -64,3 +89,4 @@ for i in range(1,col*row+1):
     plt.title(label)
     plt.axis('off')
 plt.show()
+print(  )
